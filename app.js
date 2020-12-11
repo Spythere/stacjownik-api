dbb931f9eb160189b754b25b1da11d9cfd315e08
Spythere <spythere@protonmail.com>
2020-12-11 15:32:58 +0100

    Modyfikacja ustawień CORS

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const allowedSites = ["https://stacjownik-td2.web.app"];
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, false);
+
+        if (allowedSites.indexOf(origin) == -1) {
+            const msg = `Strona ${origin} nie ma dostępu do danych tego API! W celu uzyskania dostępu skontaktuj się z Spythere na forum TD2 bądź Discordzie!`;
+            return callback(new Error(msg), false);
+        }
+
+        return callback(null, true);
+    }
+}));
 
 app.use("/api", require('./api/apiRouter'));
 
